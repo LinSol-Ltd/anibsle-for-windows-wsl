f47e99f4e374b84704fca406550e61ade7b56b29
Touqeer Hussain <touqeer.hussain@hotmail.co.uk>
2020-06-16 16:01:49 +0100

    minor corrections

--- install/install-ubuntu.ps1
+++ install/install-ubuntu.ps1
@@ -1,4 +1,4 @@
 $ProgressPreference = 'SilentlyContinue'
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.zip -UseBasicParsing
+Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile ~/Ubuntu.zip -UseBasicParsing
 Expand-Archive ~/Ubuntu.zip ~/Ubuntu
 ~/Ubuntu/ubuntu.exe
